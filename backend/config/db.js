0ef96c298219880004e34556483fef0c10eeb8d0
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-09-30 11:30:39 -0300

    Conexao com o banco mongodb

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,23 @@
+const { default: mongoose } = require('mongoose');
 const mogoose = require('mongoose');
 
 //connection
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+const conn = async () => {
+    try {
+        
+        const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@sidebitt.g7zxvhx.mongodb.net/?retryWrites=true&w=majority`);
+
+        console.log('Conectou ao banco!');
+
+        return dbConn;
+    } catch (error) {
+       console.log(error); 
+    }
+};
+
+conn();
+
+module.exports = conn;
