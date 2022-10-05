737510bee3a6fdd66c575a300420e39c5a090c13
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-10-04 22:20:36 -0300

    Resgatando o usuario  autenticado

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,15 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 // Controller
-const {register, login} = require('../controllers/UserController');
+const {register, login, getCurrentUser} = require('../controllers/UserController');
 
 // Middlewares
 const validate = require('../middlewares/handleValidation');
 const { userCreateValidation, loginValidation, 
 } = require('../middlewares/useValidations');
+const authGuard = require('../middlewares/authGuard');
 
 // Routes
 router.post('/register', userCreateValidation(), validate, register);
 router.post('/login', loginValidation(), validate, login);
+router.get('/profile', authGuard, getCurrentUser);
 
 module.exports = router;
