1a5de00c18ebcd7ddcc5ad5d662aefc7a436a029
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-10-21 19:34:12 -0300

    Estruturando pagina de edicao de perfil

---
+++ frontend/src/pages/EditProfile/EditProfile.js
@@ -0,0 +1,23 @@
+import './EditProfile.css';
+
+const EditProfile = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+  return <div id='edit-profile'>
+        <h2>Edite seus dados</h2>
+        <p className='subtitle'>Atualizar seu perfil...</p>
+        <form onSubmit={handleSubmit}>
+            <input type={"text"} placeholder="Nome" />
+            <input type={"email"} placeholder="E-mail" disabled />
+            <label>
+                <span>Quer alterar sua senha?</span>
+                <input type={"password"} placeholder="Digite sua nova senha" />
+            </label>
+            <input type={"submit"} value="Atualizar" />
+        </form>
+    </div>;
+};
+
+export default EditProfile;
