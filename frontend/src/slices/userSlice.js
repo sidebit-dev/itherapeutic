f41e9ae88efb1efed52b0a5c8c64ff038092d6a8
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-10-24 20:54:10 -0300

    Slice de atualizacao de perfil

--- frontend/src/slices/userSlice.js
+++ frontend/src/slices/userSlice.js
@@ -19,6 +19,23 @@ export const profile = createAsyncThunk(
 
         return data;
     }
+);
+
+// update user details
+export const updateProfile = createAsyncThunk(
+    "user/update",
+    async(user, thunkAPI) => {
+       const token = thunkAPI.getState.auth.user.token; 
+
+       const data = await userService.updateProfile(user, token);
+
+       // Check for error
+       if(data.errors) {
+        return thunkAPI.rejectWithValue(data.errors[0]);
+       }
+
+       return data;
+    }
 )
 
 export const userSlice = createSlice({
@@ -40,6 +57,22 @@ export const userSlice = createSlice({
             state.success = true;
             state.error = null;
             state.user = action.payload;
+        })
+        .addCase(updateProfile.pending, (state) => {
+            state.loading = true;
+            state.error = false;
+        })
+        .addCase(updateProfile.fulfilled, (state, action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+            state.user = action.payload;
+            state.message = "Usuário atualizado com sucesso!"
+        })
+        .addCase(updateProfile.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+            state.user = null;
         }); 
     },
 });
