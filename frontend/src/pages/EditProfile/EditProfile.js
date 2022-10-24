f2b9fd694b29d608885662a266246402b61433b7
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-10-24 00:19:56 -0300

    Preenchendo formulario de edicao com os dados

--- frontend/src/pages/EditProfile/EditProfile.js
+++ frontend/src/pages/EditProfile/EditProfile.js
@@ -1,19 +1,53 @@
 import './EditProfile.css';
 
+// Hooks
+import {useEffect, useState} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+
+// Redux
+import {profile, resetMessage} from '../../slices/userSlice';
+
+// Components
+import Message from '../../components/Message';
+
 const EditProfile = () => {
 
+    const dispatch = useDispatch();
+
+    const {user, message, error, loading} = useSelector((state) => state.user);
+
+   const [name, setName] = useState("");
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+
+    // Load user data
+    useEffect(() => {
+        dispatch(profile());
+    }, [dispatch]);
+
+    // Fill form with user data
+    useEffect(() => {
+
+        if(user) {
+            setName(user.name);
+            setEmail(user.email);
+        }
+    }, [user]);
+
+    console.log(user);
+
     const handleSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
     }
   return <div id='edit-profile'>
         <h2>Edite seus dados</h2>
         <p className='subtitle'>Atualizar seu perfil...</p>
         <form onSubmit={handleSubmit}>
-            <input type={"text"} placeholder="Nome" />
-            <input type={"email"} placeholder="E-mail" disabled />
+            <input type={"text"} placeholder="Nome" onChange={(e) => setName(e.target.value)} value={name || ""} />
+            <input type={"email"} placeholder="E-mail" disabled value={email || ""}  />
             <label>
                 <span>Quer alterar sua senha?</span>
-                <input type={"password"} placeholder="Digite sua nova senha" />
+                <input type={"password"} placeholder="Digite sua nova senha" onChange={(e) => setPassword(e.target.value)} value={password || ""} />
             </label>
             <input type={"submit"} value="Atualizar" />
         </form>
