19c08dc9c9156636573d924361b7f39d296608ff
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-10-25 18:36:55 -0300

    Finalizando atualizacao

--- frontend/src/pages/EditProfile/EditProfile.js
+++ frontend/src/pages/EditProfile/EditProfile.js
@@ -1,57 +1,99 @@
-import './EditProfile.css';
+import "./EditProfile.css";
 
 // Hooks
-import {useEffect, useState} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 // Redux
-import {profile, resetMessage} from '../../slices/userSlice';
+import { profile, resetMessage, updateProfile } from "../../slices/userSlice";
 
 // Components
-import Message from '../../components/Message';
+import Message from "../../components/Message";
 
 const EditProfile = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const { user, message, error, loading } = useSelector((state) => state.user);
 
-    const {user, message, error, loading} = useSelector((state) => state.user);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-   const [name, setName] = useState("");
-   const [email, setEmail] = useState("");
-   const [password, setPassword] = useState("");
+  // Load user data
+  useEffect(() => {
+    dispatch(profile());
+  }, [dispatch]);
 
-    // Load user data
-    useEffect(() => {
-        dispatch(profile());
-    }, [dispatch]);
-
-    // Fill form with user data
-    useEffect(() => {
-
-        if(user) {
-            setName(user.name);
-            setEmail(user.email);
-        }
-    }, [user]);
-
-    console.log(user);
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  // Fill form with user data
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+      setEmail(user.email);
     }
-  return <div id='edit-profile'>
-        <h2>Edite seus dados</h2>
-        <p className='subtitle'>Atualizar seu perfil...</p>
-        <form onSubmit={handleSubmit}>
-            <input type={"text"} placeholder="Nome" onChange={(e) => setName(e.target.value)} value={name || ""} />
-            <input type={"email"} placeholder="E-mail" disabled value={email || ""}  />
-            <label>
-                <span>Quer alterar sua senha?</span>
-                <input type={"password"} placeholder="Digite sua nova senha" onChange={(e) => setPassword(e.target.value)} value={password || ""} />
-            </label>
-            <input type={"submit"} value="Atualizar" />
-        </form>
-    </div>;
+  }, [user]);
+
+  console.log(user);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Gather user data from states
+    const userData = {
+        name
+    }
+
+    // parte de imagem se tiver
+
+    if(password) {
+        userData.password = password;
+    }
+
+    // build form data
+    const formData = new FormData();
+
+    const userFormData = Object.keys(userData).forEach((key) => formData.append(key, userData[key]));
+
+    formData.append("user", userFormData);
+
+    await dispatch(updateProfile(userFormData));
+
+    setTimeout(() => {
+        dispatch(resetMessage())
+    }, 2000);
+  };
+  return (
+    <div id="edit-profile">
+      <h2>Edite seus dados</h2>
+      <p className="subtitle">Atualizar seu perfil...</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type={"text"}
+          placeholder="Nome"
+          onChange={(e) => setName(e.target.value)}
+          value={name || ""}
+        />
+        <input
+          type={"email"}
+          placeholder="E-mail"
+          disabled
+          value={email || ""}
+        />
+        <label>
+          <span>Quer alterar sua senha?</span>
+          <input
+            type={"password"}
+            placeholder="Digite sua nova senha"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password || ""}
+          />
+        </label>
+        {!loading && <input type="submit" value="Atualizar" />}
+        {loading && <input type="submit" value="Aguarde..." disabled />}
+        {error && <Message msg={error} type="error" />}
+        {message && <Message msg={message} type="success" />}
+      </form>
+    </div>
+  );
 };
 
 export default EditProfile;
--- frontend/src/slices/userSlice.js
+++ frontend/src/slices/userSlice.js
@@ -25,7 +25,7 @@ export const profile = createAsyncThunk(
 export const updateProfile = createAsyncThunk(
     "user/update",
     async(user, thunkAPI) => {
-       const token = thunkAPI.getState.auth.user.token; 
+       const token = thunkAPI.getState().auth.user.token; 
 
        const data = await userService.updateProfile(user, token);
 
@@ -70,9 +70,10 @@ export const userSlice = createSlice({
             state.message = "Usuário atualizado com sucesso!"
         })
         .addCase(updateProfile.rejected, (state, action) => {
+            console.log(state, action);
             state.loading = false;
             state.error = action.payload;
-            state.user = null;
+            state.user = {};
         }); 
     },
 });
